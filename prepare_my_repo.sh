9e0b5842b41e3c322cce90867d2fa6223f07f96f
mathis.calonnec <mathiscalonnec@mathis.calonnec>
2019-10-24 16:09:55 +0200

    remove prepare_my_repo

--- prepare_my_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# prepare_my_repo.bash
-
-blih -u clement.auger@epitech.eu repository create $1
-blih -u clement.auger@epitech.eu repository setacl $1 ramassage-tek r
-git clone git@git.epitech.eu:/clement.auger@epitech.eu/$1
